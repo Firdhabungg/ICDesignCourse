518712b2622398a98be91237fd7716043d31a88c
Firdhabungg <firdhafirmansyah@dicky.gmail.com>
2025-01-16 21:59:58 +0700

    pop up konfirmasi pembayaran

--- app/src/main/java/com/example/icdesigncourse/MetodePembayaran.kt
+++ app/src/main/java/com/example/icdesigncourse/MetodePembayaran.kt
@@ -1,7 +1,10 @@
 package com.example.icdesigncourse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,5 +19,22 @@ class MetodePembayaran : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnBack = findViewById<Button>(R.id.btn_konfirmasi)
+        btnBack.setOnClickListener {
+            showPaymentSuccess()
+        }
+    }
+    private fun showPaymentSuccess() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Pembayaran Berhasil")
+        builder.setMessage("Terima kasih, pembayaran Anda telah berhasil diproses.")
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss() // Menutup popup saat tombol OK ditekan
+            val intentModul = Intent(this@MetodePembayaran, Home_activity::class.java)
+            startActivity(intentModul)
+        }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
